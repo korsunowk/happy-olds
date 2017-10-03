f3b0ea2c4c81f2fd0f26d2b83bf6a3f2a2ca6a7b
Konstantin <korsunowk@gmail.com>
2017-10-03 11:40:42 +0300

    Delete static folder

--- static/js.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('.day').click(function(){
-        var count = $(this).find('.olds-count').text().split(' ')[0];
-
-        if (count > 0) {
-            $(this).find('.olds-block').toggleClass('open');
-        }
-    });
-});
